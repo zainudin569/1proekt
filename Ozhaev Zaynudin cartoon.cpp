54bb2d7732099fc49a1870b10a21bbf639df0e17
zainudin569 <81910959+zainudin569@users.noreply.github.com>
2021-04-25 13:31:13 +0300

    C:\Users\zainu\Desktop\курс\1proekt\1proekt\Ozhaev Zaynudin cartoon.cpp -o C:\Users\zainu\Desktop\курс\1proekt\1proekt\Ozhaev Zaynudin cartoon.exe  -Wshadow -Winit-self -Wredundant-decls -Wcast-align -Wundef -Wfloat-equal -Winline -Wunreachable-code -Wmissing-declarations -Wmissing-include-dirs -Wswitch-enum -Wswitch-default -Weffc++ -Wmain -Wextra -Wall -g -pipe -fexceptions -Wcast-qual -Wconversion -Wctor-dtor-privacy -Wempty-body -Wformat-security -Wformat=2 -Wignored-qualifiers -Wlogical-op -Wmissing-field-initializers -Wnon-virtual-dtor -Woverloaded-virtual -Wpointer-arith -Wsign-promo -Wstack-usage=8192 -Wstrict-aliasing -Wstrict-null-sentinel -Wtype-limits -Wwrite-strings -D_DEBUG -D_EJUDGE_CLIENT_SIDE -D_EJC

--- Ozhaev Zaynudin cartoon.cpp
+++ Ozhaev Zaynudin cartoon.cpp
@@ -61,9 +61,9 @@ void DrawGirl  (int x, int y, double sizeX, double sizeY, COLORREF GirlColor,
     {
     txSetFillColor (GirlColor);
     txEllipse (x, y, x + 30*sizeX, y + 30*sizeY);
-    POINT Girl [3] = {{ ROUND(x + 15*sizeX), y + 30*sizeY },
-                      { x +  5*sizeX, y + 75*sizeY },
-                      { x + 25*sizeX, y + 75*sizeY }};
+    POINT Girl [3] = {{ ROUND( x + 15*sizeX ), ROUND( y + 30*sizeY ) },
+                      { ROUND( x +  5*sizeX ), ROUND( y + 75*sizeY ) },
+                      { ROUND( x + 25*sizeX ), ROUND( y + 75*sizeY ) }};
     txPolygon (Girl, 3);
     txSetColor (RGB (255, 128, 0), 1);
     txSetFillColor (RGB (255, 0, 0));
