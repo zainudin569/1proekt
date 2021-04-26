2c9d71180d91f5e109db9b9c22f08b1b8563d4a7
zainudin569 <81910959+zainudin569@users.noreply.github.com>
2021-04-27 01:35:37 +0300

    C:\Users\zainu\Desktop\курс\1proekt\1proekt\Ozhaev Zaynudin cartoon.cpp -o C:\Users\zainu\Desktop\курс\1proekt\1proekt\Ozhaev Zaynudin cartoon.exe  -Wshadow -Winit-self -Wredundant-decls -Wcast-align -Wundef -Wfloat-equal -Winline -Wunreachable-code -Wmissing-declarations -Wmissing-include-dirs -Wswitch-enum -Wswitch-default -Weffc++ -Wmain -Wextra -Wall -g -pipe -fexceptions -Wcast-qual -Wconversion -Wctor-dtor-privacy -Wempty-body -Wformat-security -Wformat=2 -Wignored-qualifiers -Wlogical-op -Wmissing-field-initializers -Wnon-virtual-dtor -Woverloaded-virtual -Wpointer-arith -Wsign-promo -Wstack-usage=8192 -Wstrict-aliasing -Wstrict-null-sentinel -Wtype-limits -Wwrite-strings -D_DEBUG -D_EJUDGE_CLIENT_SIDE -D_EJC

--- Ozhaev Zaynudin cartoon.cpp
+++ Ozhaev Zaynudin cartoon.cpp
@@ -117,7 +117,7 @@ void DrawMan  (int x, int y, double sizeX, double sizeY, COLORREF ManColor,
     {
     txSetFillColor (ManColor);
     txEllipse (x, y, ROUND( x + 30*sizeX ), ROUND( y + 30*sizeY ));
-    POINT Man [3] =  {{ ROUND( x + 15*sizeX ), ROUND( y + 75*sizeY )},
+    POINT Man [3] =  {{ ROUND( x + 5*sizeX ), ROUND( y + 75*sizeY )},
                        { ROUND( x +  25*sizeX ), ROUND( y + 75*sizeY )},
                        { ROUND( x + 15*sizeX ), ROUND( y + 30*sizeY )}};
     txPolygon (Man, 3);
