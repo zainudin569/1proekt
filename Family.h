8fc2ce812bf47c13e21f8e95a4a92ac6fe6cdb40
zainudin569 <81910959+zainudin569@users.noreply.github.com>
2021-05-06 16:09:58 +0300

    C:\Users\zainu\Desktop\курс\1proekt\1proekt\Ozhaev Zaynudin cartoon.cpp -o C:\Users\zainu\Desktop\курс\1proekt\1proekt\Ozhaev Zaynudin cartoon.exe  -Wshadow -Winit-self -Wredundant-decls -Wcast-align -Wundef -Wfloat-equal -Winline -Wunreachable-code -Wmissing-declarations -Wmissing-include-dirs -Wswitch-enum -Wswitch-default -Weffc++ -Wmain -Wextra -Wall -g -pipe -fexceptions -Wcast-qual -Wconversion -Wctor-dtor-privacy -Wempty-body -Wformat-security -Wformat=2 -Wignored-qualifiers -Wlogical-op -Wmissing-field-initializers -Wnon-virtual-dtor -Woverloaded-virtual -Wpointer-arith -Wsign-promo -Wstack-usage=8192 -Wstrict-aliasing -Wstrict-null-sentinel -Wtype-limits -Wwrite-strings -D_DEBUG -D_EJUDGE_CLIENT_SIDE -D_EJC

--- Family.h
+++ Family.h
@@ -221,7 +221,7 @@ void DrawFamily ()
             int y = 200 - t*2;
             double sizeX = 0.006*t;
             double sizeY = 0.0037*t;
-            txSetColor     (RGB (255, 0, 0), 4);
+            txSetColor     (RGB (255, 0, 0), 5);
             txSetFillColor (RGB (255, 0, 0));
             POINT ArmBoy [3] =  {{ ROUND( x + 413*sizeX ), ROUND( y + 464*sizeY )},
                                  { ROUND( x + 371*sizeX ), ROUND( y + 396*sizeY )},
