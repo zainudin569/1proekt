f5602f0dce57acabafb7a612c5d2d641bff166bc
zainudin569 <81910959+zainudin569@users.noreply.github.com>
2021-05-06 17:57:56 +0300

    C:\Users\zainu\Desktop\курс\1proekt\1proekt\Ozhaev Zaynudin cartoon.cpp -o C:\Users\zainu\Desktop\курс\1proekt\1proekt\Ozhaev Zaynudin cartoon.exe  -Wshadow -Winit-self -Wredundant-decls -Wcast-align -Wundef -Wfloat-equal -Winline -Wunreachable-code -Wmissing-declarations -Wmissing-include-dirs -Wswitch-enum -Wswitch-default -Weffc++ -Wmain -Wextra -Wall -g -pipe -fexceptions -Wcast-qual -Wconversion -Wctor-dtor-privacy -Wempty-body -Wformat-security -Wformat=2 -Wignored-qualifiers -Wlogical-op -Wmissing-field-initializers -Wnon-virtual-dtor -Woverloaded-virtual -Wpointer-arith -Wsign-promo -Wstack-usage=8192 -Wstrict-aliasing -Wstrict-null-sentinel -Wtype-limits -Wwrite-strings -D_DEBUG -D_EJUDGE_CLIENT_SIDE -D_EJC

--- Ozhaev Zaynudin cartoon.cpp
+++ Ozhaev Zaynudin cartoon.cpp
@@ -361,10 +361,14 @@ void DrawZoomHouse1()
 int t = 0;
     while ( t <= 100 )
         {
+        txBegin();
+
         txSetFillColor (TX_BLACK);
         txClear ();
         DrawHouse1 (320+t*14.2, 260+t*22.5, 1+t*0.24, 1+t*0.2, RGB (0, 80, 80));
         t ++;
         txSleep (100);
+
+        txEnd();
         }
 }
