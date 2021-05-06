96d3b6dae91d3a6112994ffec4a4eaee9248d74a
zainudin569 <81910959+zainudin569@users.noreply.github.com>
2021-05-06 15:48:08 +0300

    C:\Users\zainu\Desktop\курс\1proekt\1proekt\Ozhaev Zaynudin cartoon.cpp -o C:\Users\zainu\Desktop\курс\1proekt\1proekt\Ozhaev Zaynudin cartoon.exe  -Wshadow -Winit-self -Wredundant-decls -Wcast-align -Wundef -Wfloat-equal -Winline -Wunreachable-code -Wmissing-declarations -Wmissing-include-dirs -Wswitch-enum -Wswitch-default -Weffc++ -Wmain -Wextra -Wall -g -pipe -fexceptions -Wcast-qual -Wconversion -Wctor-dtor-privacy -Wempty-body -Wformat-security -Wformat=2 -Wignored-qualifiers -Wlogical-op -Wmissing-field-initializers -Wnon-virtual-dtor -Woverloaded-virtual -Wpointer-arith -Wsign-promo -Wstack-usage=8192 -Wstrict-aliasing -Wstrict-null-sentinel -Wtype-limits -Wwrite-strings -D_DEBUG -D_EJUDGE_CLIENT_SIDE -D_EJC

--- Family.h
+++ Family.h
@@ -226,8 +226,8 @@ void DrawFamily ()
                              { ROUND( x + 455*sizeX ), ROUND( y + 396*sizeY )}};
         txPolygon (ArmBoy, 3);
 
-        txEllipse   (ROUND( x + 365*sizeX ), ROUND( y + 355*sizeY ),
-                     ROUND( x + 415*sizeX ), ROUND( y + 405*sizeY ));
+        txEllipse   (ROUND( x + 363*sizeX ), ROUND( y + 353*sizeY ),
+                     ROUND( x + 413*sizeX ), ROUND( y + 403*sizeY ));
 
         txEllipse   (ROUND( x + 413*sizeX ), ROUND( y + 353*sizeY ),
                      ROUND( x + 463*sizeX ), ROUND( y + 403*sizeY ));
